8fbf091783cf793c46220ecef6ab7d60f1b9cb3b
Deexith Madas <deexithmadas772@gmail.com>
2023-12-05 15:10:42 +0530

    update index.js

--- index.js
+++ index.js
@@ -8,20 +8,10 @@ const app = express();
 const port = process.env.port || 5000;
 const client = db.client;
 
-const allowedUrls = ["http://localhost:3000", "https://heliverse-frontend-lfxx.vercel.app"];
-const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedUrls.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error())
-      }
-    },
-    credentials: true
-  }
-
+app.use(cors());
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+
+
 
 app.use('/api/users', userRouter);
 
