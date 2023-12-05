24dd5d27c67241f1b24ecbddc408d4fe4125b8f6
Deexith Madas <deexithmadas772@gmail.com>
2023-12-05 14:47:20 +0530

    Adding all

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("./db");
+const userRouter = require("./user");
+
+const app = express();
+const port = process.env.port || 5000;
+const client = db.client;
+
+const allowedUrls = ["http://localhost:3000", "https://heliverse-frontend-lfxx.vercel.app"];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedUrls.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+app.use('/api/users', userRouter);
+
+
+
+
+app.listen(port, () => console.log("Server is running at", port));
